fd0a5cf4346adb468f5e12119c8b3069e8a06703
Colin Politi <urboycolinthepanda@gmail.com>
2026-04-29 19:27:43 -0400

    add file execution

--- pdvm.c
+++ pdvm.c
@@ -81,8 +81,7 @@ void vm_swap(Vm *vm)
 
 void vm_print(Vm *vm)
 {
-    printf(NUM_FMT, da_last(vm));
-    printf("\n");
+    printf(NUM_FMT"\n", da_last(vm));
 }
 
 void vm_dump(Vm *vm)
@@ -166,226 +165,265 @@ void vm_not(Vm *vm)
     vm_push(vm, vm_pop(vm) == 0);
 }
 
-int exec_line(Vm *vm, char *line)
+bool sv_eq_ignore_case(String_View a, const char *b)
 {
-    char *command = strtok(line, " \n");
-    if (!command)
-        return 1;
+    size_t b_len = strlen(b);
 
-    for (NUM i = 0; command[i]; i++)
+    if (a.count != b_len) return false;
+
+    for (size_t i = 0; i < a.count; i++)
     {
-        command[i] = tolower((unsigned char)command[i]);
+        char ca = tolower((unsigned char)a.data[i]);
+        char cb = tolower((unsigned char)b[i]);
+
+        if (ca != cb) return false;
     }
 
-    if (strcmp(command, "halt") == 0)
+    return true;
+}
+
+int exec_line(Vm *vm, String_View line)
+{
+    line = sv_trim(line);
+
+    if (line.count == 0)
+        return 1;
+
+    if (line.data[0] == '#')
+        return 1;
+
+    String_View command = sv_chop_by_delim(&line, ' ');
+    command = sv_trim(command);
+    line = sv_trim(line);
+
+    if (sv_eq_ignore_case(command, "halt"))
     {
         return 2;
     }
-    else if (strcmp(command, "push") == 0)
+    else if (sv_eq_ignore_case(command, "push"))
     {
-        char *num_str = strtok(NULL, " \n");
-        if (num_str)
-        {
-            vm_push(vm, atoi(num_str));
-        }
-        else
+        if (line.count == 0)
         {
             printf("push requires a number\n");
             return 1;
         }
+
+        const char *num_str = temp_sv_to_cstr(line);
+        vm_push(vm, atoll(num_str));
     }
-    else if (strcmp(command, "pop") == 0)
+    else if (sv_eq_ignore_case(command, "pop"))
     {
         if (vm->count == 0)
         {
             printf("Stack is empty\n");
             return 1;
         }
+
         vm_pop(vm);
     }
-    else if (strcmp(command, "print") == 0)
+    else if (sv_eq_ignore_case(command, "print"))
     {
         if (vm->count == 0)
         {
             printf("Stack is empty\n");
             return 1;
         }
+
         vm_print(vm);
     }
-    else if (strcmp(command, "add") == 0)
+    else if (sv_eq_ignore_case(command, "add"))
     {
         if (vm->count < 2)
         {
             printf("There is less than 2 numbers on the stack\n");
             return 1;
         }
+
         vm_add(vm);
     }
-    else if (strcmp(command, "sub") == 0)
+    else if (sv_eq_ignore_case(command, "sub"))
     {
         if (vm->count < 2)
         {
             printf("There is less than 2 numbers on the stack\n");
             return 1;
         }
+
         vm_sub(vm);
     }
-    else if (strcmp(command, "mul") == 0)
+    else if (sv_eq_ignore_case(command, "mul"))
     {
         if (vm->count < 2)
         {
             printf("There is less than 2 numbers on the stack\n");
             return 1;
         }
+
         vm_mul(vm);
     }
-    else if (strcmp(command, "div") == 0)
+    else if (sv_eq_ignore_case(command, "div"))
     {
         if (vm->count < 2)
         {
             printf("There is less than 2 numbers on the stack\n");
             return 1;
         }
+
         vm_div(vm);
     }
-    else if (strcmp(command, "mod") == 0)
+    else if (sv_eq_ignore_case(command, "mod"))
     {
         if (vm->count < 2)
         {
             printf("There is less than 2 numbers on the stack\n");
             return 1;
         }
+
         vm_mod(vm);
     }
-    else if (strcmp(command, "exp") == 0)
+    else if (sv_eq_ignore_case(command, "exp"))
     {
         if (vm->count < 2)
         {
             printf("There is less than 2 numbers on the stack\n");
             return 1;
         }
+
         vm_exp(vm);
     }
-    else if (strcmp(command, "swap") == 0)
+    else if (sv_eq_ignore_case(command, "swap"))
     {
         if (vm->count < 2)
         {
             printf("There is less than 2 numbers on the stack\n");
             return 1;
         }
+
         vm_swap(vm);
     }
-    else if (strcmp(command, "dup") == 0)
+    else if (sv_eq_ignore_case(command, "dup"))
     {
         if (vm->count == 0)
         {
-            printf("There needs to at least one number on the stack\n");
+            printf("There needs to be at least one number on the stack\n");
             return 1;
         }
+
         vm_dup(vm);
     }
-    else if (strcmp(command, "dump") == 0)
+    else if (sv_eq_ignore_case(command, "dump"))
     {
         vm_dump(vm);
     }
-    else if (strcmp(command, "clear") == 0)
+    else if (sv_eq_ignore_case(command, "clear"))
     {
         vm_clear(vm);
     }
-    else if (strcmp(command, "neg") == 0)
+    else if (sv_eq_ignore_case(command, "neg"))
     {
         if (vm->count == 0)
         {
-            printf("There needs to at least one number on the stack\n");
+            printf("There needs to be at least one number on the stack\n");
             return 1;
         }
+
         vm_neg(vm);
     }
-    else if (strcmp(command, "eq") == 0)
+    else if (sv_eq_ignore_case(command, "eq"))
     {
         if (vm->count < 2)
         {
             printf("There is less than 2 numbers on the stack\n");
             return 1;
         }
+
         vm_eq(vm);
     }
-    else if (strcmp(command, "neq") == 0)
+    else if (sv_eq_ignore_case(command, "neq"))
     {
         if (vm->count < 2)
         {
             printf("There is less than 2 numbers on the stack\n");
             return 1;
         }
+
         vm_neq(vm);
     }
-    else if (strcmp(command, "lt") == 0)
+    else if (sv_eq_ignore_case(command, "lt"))
     {
         if (vm->count < 2)
         {
             printf("There is less than 2 numbers on the stack\n");
             return 1;
         }
+
         vm_lt(vm);
     }
-    else if (strcmp(command, "gt") == 0)
+    else if (sv_eq_ignore_case(command, "gt"))
     {
         if (vm->count < 2)
         {
             printf("There is less than 2 numbers on the stack\n");
             return 1;
         }
+
         vm_gt(vm);
     }
-    else if (strcmp(command, "lte") == 0)
+    else if (sv_eq_ignore_case(command, "lte"))
     {
         if (vm->count < 2)
         {
             printf("There is less than 2 numbers on the stack\n");
             return 1;
         }
+
         vm_lte(vm);
     }
-    else if (strcmp(command, "gte") == 0)
+    else if (sv_eq_ignore_case(command, "gte"))
     {
         if (vm->count < 2)
         {
             printf("There is less than 2 numbers on the stack\n");
             return 1;
         }
+
         vm_gte(vm);
     }
-    else if (strcmp(command, "and") == 0)
+    else if (sv_eq_ignore_case(command, "and"))
     {
         if (vm->count < 2)
         {
             printf("There is less than 2 numbers on the stack\n");
             return 1;
         }
+
         vm_and(vm);
     }
-    else if (strcmp(command, "or") == 0)
+    else if (sv_eq_ignore_case(command, "or"))
     {
         if (vm->count < 2)
         {
             printf("There is less than 2 numbers on the stack\n");
             return 1;
         }
+
         vm_or(vm);
     }
-    else if (strcmp(command, "not") == 0)
+    else if (sv_eq_ignore_case(command, "not"))
     {
         if (vm->count == 0)
         {
-            printf("There needs to at least one number on the stack\n");
+            printf("There needs to be at least one number on the stack\n");
             return 1;
         }
+
         vm_not(vm);
     }
     else
     {
         printf("Command not found\n");
     }
+
     return 1;
 }
 
@@ -397,9 +435,32 @@ int console(Vm *vm)
         printf("vm> ");
         if (!fgets(input, sizeof(input), stdin))
             break;
-        if (exec_line(vm, input) == 2)
+        if (exec_line(vm, sv_from_cstr(input)) == 2)
             return 0;
     }
+    return 0;
+}
+
+int exec_file(Vm *vm, char *filepath) {
+    String_Builder file = {0};
+    if (!read_entire_file(filepath, &file)) {
+        printf("Failed to read file\n");
+        return 1;
+    }
+
+    String_View content = sb_to_sv(file);
+
+    while (content.count > 0) {
+        String_View line = sv_chop_by_delim(&content, '\n');
+
+        line = sv_trim(line);
+
+        if (line.count == 0) continue;
+        if (line.data[0] == '#') continue;
+        if (exec_line(vm, line) == 2) break;
+    }
+
+    sb_free(file);
     return 0;
 }
 
@@ -414,7 +475,7 @@ int main(int argc, char *argv[])
     else
     {
         char *file_path = argv[1];
-        printf("Running file %s", file_path);
+        exec_file(&vm, file_path);
     }
 
     return 0;
